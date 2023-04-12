8f3e4eb6eceaa546178d049a60f14811c35bd1b4
Manolo-AE <124036561+Manolo-AE@users.noreply.github.com>
2023-04-12 10:21:34 +0200

    our latest changes

--- test.php
+++ test.php
@@ -25,7 +25,7 @@ $answerSelectQurey = $qp->selectQuery($selectQuery);
 ?>
 
 <div class="container pt-3">
-    <h3>DAA AE Ina Zimmermann</h3>
+    <h3>DAA AE und Master</h3>
     <?=var_dump($answerSelectQurey)?>
 </div>
 

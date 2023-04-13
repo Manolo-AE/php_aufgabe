6b0e1a1539f2b8bda5da7b3e571b4402d9109667
Manolo-AE <124036561+Manolo-AE@users.noreply.github.com>
2023-04-13 12:02:02 +0200

    weitere textänderungen

--- shop.php
+++ shop.php
@@ -1,9 +1,13 @@
 <?php include('./template/navbar.php'); ?>
 
 <div class="container pt-3">
+    <br>
     <h3>Shop</h3>
+    <br>
     <h4>-hier entsteht unsere Shop seite-</h4>
-    <h4>sollten Sie interesse haben, mitzuwirken, bewerben Sie sich einfach unter bewerbung@test.com</h4>
+    <br>
+    <h4>Sollten Sie interesse haben, mitzuwirken,</h4>
+    <h4>bewerben Sie sich einfach unter bewerbung@test.com</h4>
 </div>
 
 
